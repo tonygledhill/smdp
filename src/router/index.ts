9d6d11124273dede877425b9e8725ad95a9c9597
Tony Gledhill <tonygledhilldevelopment@gmail.com>
2023-01-29 15:58:55 +0000

    Added base pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue';
+import ConsultancyView from '../views/Consultancy.vue';
+import ContactView from '../views/Contact.vue';
+import SalesCampaignsView from '../views/SalesCampaigns.vue';
+import TrainingProgrammesView from '../views/TrainingProgrammes.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,13 +15,30 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
+      path: '/',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      component: AboutView
+    },
+    {
+      path: '/',
+      name: 'consultancy',
+      component: ConsultancyView
+    },
+    {
+      path: '/',
+      name: 'contact',
+      component: ContactView
+    },
+    {
+      path: '/',
+      name: 'sales-campaigns',
+      component: SalesCampaignsView
+    },
+    {
+      path: '/',
+      name: 'training-programmes',
+      component: TrainingProgrammesView
+    },
   ]
 })
 
